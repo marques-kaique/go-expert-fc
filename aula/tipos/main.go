3669910c0a98e94bdf8d997e374a40e58656092b
k <kaiquesmarques@gmail.com>
2025-07-02 15:01:15 -0300

    aula: generics pt 1

--- aula/tipos/main.go
+++ aula/tipos/main.go
@@ -5,21 +5,11 @@ import (
 	"unsafe"
 )
 
-type ID int
-
-type User struct {
-	ID     int
-	Name   string
-	Active bool
-}
-
 var (
 	a string
 	b bool
 	c int
 	d float64
-	e ID
-	f User
 	g [2]int // array valor fixo
 	h []int  // slice valor variável
 	i map[string]int
@@ -34,27 +24,6 @@ func main() {
 	println("float64: ", d, "tamanho: ", unsafe.Sizeof(d))
 	fmt.Printf("%T  tamanho: %v\n", d, unsafe.Sizeof(d))
 	fmt.Printf("%T  tamanho: %d\n\n", d, unsafe.Sizeof(d))
-
-	println("\n\n*** ID - Novo tipo")
-	println("ID: ", e, "tamanho: ", unsafe.Sizeof(e))
-
-	print("\n\n*** Struct\n")
-	fmt.Printf("%T: %v tamanho: %v\n", f, f, unsafe.Sizeof(f))
-	fmt.Printf("%T: %v tamanho: %v\n\n", f.ID, f.ID, unsafe.Sizeof(f.ID))
-
-	//inicializando um struct
-	user := User{
-		ID:     1,
-		Name:   "John",
-		Active: true,
-	}
-
-	print("*** Struct inicializada\n")
-	fmt.Printf("%T: %v tamanho: %v\n", user, user, unsafe.Sizeof(user))
-
-	user.Active = false
-	print("*** Struct alterada\n")
-	fmt.Printf("%T: %v tamanho: %v\n", user, user, unsafe.Sizeof(user))
 
 	println("\n\n*** Array")
 	fmt.Printf("%T: %v tamanho: %v\n\n", g, g, unsafe.Sizeof(g))
